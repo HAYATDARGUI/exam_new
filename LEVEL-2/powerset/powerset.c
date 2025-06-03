fd185ec0367b21b32d899b62e3ba17846615e096
Hayat Dargui <hdargui@c1r5p9.1337.ma>
2025-06-03 20:50:24 +0100

    up2

--- LEVEL-2/powerset/powerset.c
+++ LEVEL-2/powerset/powerset.c
@@ -6,75 +6,83 @@
 /*   By: hdargui <hdargui@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/01 13:12:44 by mlaffita          #+#    #+#             */
-/*   Updated: 2025/05/27 20:13:27 by hdargui          ###   ########.fr       */
+/*   Updated: 2025/06/03 20:48:58 by hdargui          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <unistd.h>
+#include <stdlib.h> 
 #include <stdio.h>
-#include <stdlib.h>
-// int simple_atoi(char *str) {
-//     int result = 0;
-//     int sign = 1;
-//     if (*str == '-') {
-//         sign = -1;
-//         str++;
-//     }
-
-//     while (*str) {
-//         result = result * 10 + (*str - '0');
-//         str++;
-//     }
-
-//     return result * sign;
-// }
-
-void print_subset(int *subset, int size) {
-    for (int i = 0; i < size; i++) {
-        printf("%d ", subset[i]);
-    }
-    printf("\n");
-}
-
-void find_subsets(int *arr, int n, int target, int *subset, int size, int index)
+int numberdejavu(int *tabnumbers,int indextab,int *tabfinal,int indextabf)
 {
-    if (index == target )
+    int i=0;
+    int j=0;
+    while(i <indextab && j<indextabf)
     {
-        print_subset(subset, size);
-        return;
+        if(tabnumbers[i]!=tabfinal[j])
+            i++;
+        if(tabnumbers[i]==tabfinal[j])
+        {
+            i++;
+            j++;
+        }
     }
-    if (index == 0 || target < 0) return;
-
-    subset[size] = arr[index];
-    find_subsets(arr, n, target - arr[index], subset, size + 1, index + 1);
-    find_subsets(arr, n, target, subset, size, index + 1);
-}
-
-int main(int argc, char **argv) {
-    printf("ghere");
-    if (argc < 3) return 1;
-    int target = atoi(argv[1]);
-    int arr[argc - 2];
-    
-    for (int i = 2; i < argc; i++) {
-        arr[i - 2] = atoi(argv[i]);
-    }
-
-    int subset[argc - 2];
-    find_subsets(arr, argc - 2, target, subset, 0, 0);
-
+    if(j==indextabf)
+        return 1;
     return 0;
 }
-
-
-// void ft_swap(int *a,int *b)
-// {
-//     int tmp=*a;
-//     *a=*b;
-//     *b=*a;
-// }
-
-// void help(int *tab,int n)
-// {
-    
-// }
+void powerset(int target,int *tabnumbers,int indextab,int *tabfinal,int indextabf,int *usednumber)
+{
+    if(target==0)
+    {
+        if(numberdejavu(tabnumbers,indextab,tabfinal,indextabf)==1)
+        {
+            for(int i=0;i<indextabf;i++)
+                printf("%d",tabfinal[i]);
+            printf("\n");
+        }
+        return ;
+    }
+    for(int i=0;i< indextab;i++)
+    {
+        if(usednumber[i]==0)
+        {
+            usednumber[i]=1;
+            target-=tabnumbers[i];
+            tabfinal[indextabf]=tabnumbers[i];
+            powerset(target,tabnumbers,indextab,tabfinal,indextabf+1,usednumber);
+            usednumber[i]=0;
+            target+=tabnumbers[i];
+        }
+    }
+}
+int main(int arc,char **arv)
+{
+    if(arc < 3)
+        return -1;
+    int target =atoi(arv[1]);
+    int indextab= arc-2;
+    int *tabnumbers=malloc(sizeof(int)*indextab);
+    if(!tabnumbers)
+    {
+        exit(1);
+    }
+    for(int i=0;i < indextab;i++)
+    {
+        tabnumbers[i]=atoi(arv[i+2]);
+    }
+    int *tabfinal=malloc(sizeof(int)*indextab);
+    // printf("%d",*tabfinal);
+    if(!tabfinal)
+    {
+        exit(1);
+    }
+    int *usednumber=calloc(sizeof(int),indextab);
+    if(!usednumber)
+    {
+        exit(1);
+    }
+    powerset(target,tabnumbers,indextab,tabfinal,0,usednumber);
+    free(tabfinal);
+    free(tabnumbers);
+    free(usednumber);
+}
